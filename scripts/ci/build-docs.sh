d96d9c373e90d66325bfc5784a654b9490c095fc
Anas Nashif <anas.nashif@intel.com>
2017-04-29 16:54:52 -0400

    build docs
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/ci/build-docs.sh
+++ scripts/ci/build-docs.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 set -e
 
-sudo pip install pygithub
-echo "- Checkpatch"
-cd ${ZEPHYRREPO_STATE}
-source zephyr-env.sh
-git diff ${ZEPHYR_CIREPO_VERSIONNAME} | ${ZEPHYR_BASE}/scripts/checkpatch.pl --mailback --no-tree
-
 echo "- Install dependencies"
 sudo apt-get install doxygen make
 sudo pip install breathe sphinx
 
+cd ${ZEPHYRREPO_STATE}
+source zephyr-env.sh
+
 echo "- Building docs..."
 make htmldocs > doc.log 2>&1
 echo "- Look for new warnings..."
-#./scripts/filter-known-issues.py --config-dir .known-issues/doc/ doc.log > doc.warnings
-#cat doc.warnings
-#test -s doc.warnings && exit 0 # FIXME
+./scripts/filter-known-issues.py --config-dir .known-issues/doc/ doc.log > doc.warnings
+cat doc.warnings
+test -s doc.warnings && exit 0 
 
