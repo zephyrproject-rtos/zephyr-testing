e9c7ddf0cd7d09de1726c6557497ebdf3cbb8e64
Anas Nashif <anas.nashif@intel.com>
2017-04-29 20:25:53 -0400

    ci: upload built documnetation to AWS S3
    Also add slack integration for CI
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/ci/build-docs.sh
+++ scripts/ci/build-docs.sh
@@ -4,14 +4,12 @@ set -e
 echo "- Install dependencies"
 sudo apt-get install doxygen make
 sudo pip install breathe sphinx
+sudo pip install awscli
 
 cd ${ZEPHYRREPO_STATE}
 source zephyr-env.sh
 
 echo "- Building docs..."
 make htmldocs > doc.log 2>&1
-echo "- Look for new warnings..."
-./scripts/filter-known-issues.py --config-dir .known-issues/doc/ doc.log > doc.warnings
-cat doc.warnings
-test -s doc.warnings && exit 0 
-
+echo "Uploading to AWS S3"
+aws s3 sync --delete doc/_build/html s3://zephyr-docs/online/dev
