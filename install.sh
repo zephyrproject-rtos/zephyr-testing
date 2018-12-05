85ca21d6ec3c0634611f51d150212e05bc66b239
Anas Nashif <anas.nashif@intel.com>
2018-12-04 19:56:57 -0500

    fix caching
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ sudo apt-get install --no-install-recommends -y \
 	build-essential \
 	ccache \
 	cmake \
-	device-tree-compiler \
 	doxygen \
 	gcc-multilib \
 	gperf \
@@ -17,9 +16,6 @@ sudo apt-get install --no-install-recommends -y \
 
 sudo pip3 install -r scripts/requirements.txt
 sudo pip3 install wheel west sh
-
-sudo apt-get --no-install-recommends -y remove python3.4
-sudo apt-get --no-install-recommends -y autoremove
 
 if [ ! -d "/home/semaphore/zephyr-sdk-0.9.5" ]; then
 wget -q "https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${ZSDK_VERSION}/zephyr-sdk-${ZSDK_VERSION}-setup.run" && \
